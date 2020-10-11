7e09d4ba0eb515692153d22fccc92d34a4e1218e
Charles Kornoelje <charleskornoelje@gmail.com>
2020-10-11 15:19:51 -0400

    fix schedule to respect course assignments

--- schedule.py
+++ schedule.py
@@ -1,34 +1,27 @@
-from csp import backtracking_search, CSP
+from tools.aima.csp import backtracking_search, CSP
 import itertools
 
-# create list of variables,
-classes = ["cs108", "cs112", "cs212", "cs214", "cs262", "cs232"]
 
-# each variable and their neighbors
-neighbors = {
-    "cs108": ["cs112", "cs212", "cs214", "cs262", "cs232"],
-    "cs112": ["cs108", "cs212", "cs214", "cs262", "cs232"],
-    "cs212": ["cs108", "cs112", "cs214", "cs262", "cs232"],
-    "cs214": ["cs108", "cs112", "cs212", "cs262", "cs232"],
-    "cs262": ["cs108", "cs112", "cs212", "cs214", "cs232"],
-    "cs232": ["cs108", "cs112", "cs212", "cs214", "cs262"]
-}
-
-attributes = {
-    "faculty": ["schuurman", "adams", "vanderlinden", "norman"],
-    "time": ["mwf900", "mwf1030", "mwf1130", "tth1030"],
-    "room": ["nh253", "sb382"]
-}
-
-# referenced for itertools: https://stackoverflow.com/questions/798854/all-combinations-of-a-list-of-lists
-attribute_list = [attributes["time"], attributes["room"], attributes["faculty"]]
-possible_values = list(itertools.product(*attribute_list))
-
-# referenced for function mapping: https://www.geeksforgeeks.org/python-map-function/
-domains = dict(map(lambda aClass: (aClass, possible_values[:]), classes))
+def get_neighbors(vars_list):
+    """Return a dict of neighbors, where for each class a
+    list of all other classes are presented"""
+    res = {}
+    for var in vars_list:
+        res[var] = [var2 for var2 in vars_list if var2 != var]
+    return res
 
 
-def constraints(Class1, c1, Class2, c2):
+def respect_assignments(a_class, possible_value_tuples, assignments):
+    """Limit the domain for a class to only consist of possible
+    (time, room, faculty) combinations where faculty is the same
+    on the assignment"""
+    prof = assignments[a_class]
+    limited_domain = [value for value in possible_value_tuples if value[2] == prof]
+
+    return a_class, limited_domain
+
+
+def constraints(class1, c1, class2, c2):
     """Constraints for class scheduling
     c1 and c2 are tuples in the form (time, room, faculty)
     returns true if constraints are met.
@@ -36,7 +29,7 @@ def constraints(Class1, c1, Class2, c2):
     is implicit because classes are variables.
     """
     # Return true if same class.
-    if Class1 == Class2:
+    if class1 == class2:
         return True
 
     # check to make sure faculty is not teaching at the same time
@@ -49,12 +42,38 @@ def constraints(Class1, c1, Class2, c2):
     return True
 
 
-# Setup problem
-problem = CSP(classes, domains, neighbors, constraints)
+# problem variables
+variables = classes = ["cs108", "cs112",
+                       "cs212", "cs214", "cs262", "cs232", "cs344"]
 
+# each variable and their neighbors
+neighbors = get_neighbors(variables)
+
+# attributes for constraint problem
+attributes = {
+    "faculty": ["schuurman", "adams", "plantinga", "vanderlinden", "norman"],
+    "assignments": {"cs108": "schuurman", "cs112": "adams", "cs212": "plantinga", "cs214": "adams",
+                    "cs232": "norman", "cs262": "vanderlinden", "cs344": "vanderlinden"},
+    "time": ["mwf900", "mwf1030", "mwf1130", "tth1030"],
+    "room": ["nh253", "sb382"]
+}
+
+# create list of lists and make a cartesian product of them
+attribute_list = [attributes["time"], attributes["room"],
+                  attributes["faculty"]]
+possible_values = list(itertools.product(*attribute_list))
+
+# get all possible domains
+domains = dict(map(lambda a_class: respect_assignments(a_class, possible_values, attributes["assignments"]), classes))
+
+# Setup problem
+problem = CSP(variables, domains, neighbors, constraints)
 solution = backtracking_search(problem)
 
-# Adapted from u03constraint/queens.py
+# print result
+print('Variables: ' + str(variables))
+print('Domains: ' + str(domains))
+print('Neighbors: ' + str(neighbors))
 if solution is not None and problem.goal_test(solution):
     print('Solution:')
     print(solution)
