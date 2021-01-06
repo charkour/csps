7d427c9b2a65f5cdb6a823245a9b18506db853ef
Charles Kornoelje <charleskornoelje@gmail.com>
2021-01-05 22:19:23 -0500

    convert configs to json

--- .np-config.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  yarn: false,
-  contents: "dist",
-  branch: "main",
-};
--- .prettierrc.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  trailingComma: "all",
-  tabWidth: 2,
-  useTabs: false,
-  semi: true,
-  singleQuote: false,
-  printWidth: 100,
-  bracketSpacing: true,
-};
