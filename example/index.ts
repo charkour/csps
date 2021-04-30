4e5bb8519426a1af967f82972837f620b303062e
Charles Kornoelje <33156025+charkour@users.noreply.github.com>
2021-04-30 12:26:30 -0400

    update example

--- example/index.ts
+++ example/index.ts
@@ -1,28 +1,35 @@
-import { CSP, min_conflicts } from "csps";
+import { CSP, CurrentDomain, min_conflicts } from "csps";
+
+// Define your variable's object attribute structure
+interface ClassAttributes {
+  faculty: string;
+  room: string;
+  time: string;
+}
 
 // Setup your problem
-const variables = ["cs108", "cs112", "cs212", "cs214"];
-const possible_attributes = [
-  ["mwf800", "nh253", "norman"],
-  ["mwf800", "nh253", "vanderlinden"],
-  ["mwf800", "nh253", "adams"],
-  ["mwf800", "sb382", "norman"],
-  ["mwf800", "sb382", "vanderlinden"],
-  ["mwf800", "sb382", "adams"],
-  ["mwf900", "nh253", "norman"],
-  ["mwf900", "nh253", "vanderlinden"],
-  ["mwf900", "nh253", "adams"],
-  ["mwf900", "sb382", "norman"],
-  ["mwf900", "sb382", "vanderlinden"],
-  ["mwf900", "sb382", "adams"],
+const variables: string[] = ["cs108", "cs112", "cs212", "cs214"];
+const possible_attributes: ClassAttributes[] = [
+  { time: "mwf800", room: "nh253", faculty: "norman" },
+  { time: "mwf800", room: "nh253", faculty: "vanderlinden" },
+  { time: "mwf800", room: "nh253", faculty: "adams" },
+  { time: "mwf800", room: "sb382", faculty: "norman" },
+  { time: "mwf800", room: "sb382", faculty: "vanderlinden" },
+  { time: "mwf800", room: "sb382", faculty: "adams" },
+  { time: "mwf900", room: "nh253", faculty: "norman" },
+  { time: "mwf900", room: "nh253", faculty: "vanderlinden" },
+  { time: "mwf900", room: "nh253", faculty: "adams" },
+  { time: "mwf900", room: "sb382", faculty: "norman" },
+  { time: "mwf900", room: "sb382", faculty: "vanderlinden" },
+  { time: "mwf900", room: "sb382", faculty: "adams" },
 ];
-const domains = {
+const domains: { [key: string]: ClassAttributes[] } = {
   cs108: possible_attributes,
   cs112: possible_attributes,
   cs212: possible_attributes,
   cs214: possible_attributes,
 };
-const neighbors = {
+const neighbors: { [key: string]: string[] } = {
   cs108: ["cs112", "cs212", "cs214"],
   cs112: ["cs108", "cs212", "cs214"],
   cs212: ["cs108", "cs112", "cs214"],
@@ -30,13 +37,13 @@ const neighbors = {
 };
 const constraints = (
   class1: string,
-  c1Attributes: string[],
+  c1Attributes: ClassAttributes,
   class2: string,
-  c2Attributes: string[],
+  c2Attributes: ClassAttributes,
 ): boolean => {
   /*
     Constraints for class scheduling
-    c1 and c2 are tuples in the form (time, room, faculty)
+    c1 and c2 are objects in the form {time, room, faculty}
     returns true if constraints are met.
     The constraint that there is only one section of class
     is implicit because classes are variables.
@@ -47,25 +54,30 @@ const constraints = (
   }
 
   // Check to make sure faculty is not teaching at the same time
-  if (c1Attributes[0] === c2Attributes[0] && c1Attributes[2] === c2Attributes[2]) {
+  if (c1Attributes.time === c2Attributes.time && c1Attributes.faculty === c2Attributes.faculty) {
     return false;
   }
 
   // Check to make sure class is not in the same room at the same time
-  if (c1Attributes[0] === c2Attributes[0] && c1Attributes[1] === c2Attributes[1]) {
+  if (c1Attributes.time === c2Attributes.time && c1Attributes.room === c2Attributes.room) {
     return false;
   }
   return true;
 };
 
-const aCSP = new CSP<string>(variables, domains, neighbors, constraints);
+const aCSP: CSP<ClassAttributes> = new CSP<ClassAttributes>(
+  variables,
+  domains,
+  neighbors,
+  constraints,
+);
 
 // run min_conflicts on problem
-const res = min_conflicts(aCSP);
+const res: CurrentDomain<ClassAttributes> = min_conflicts(aCSP);
 console.log(res);
 // {
-//   cs108: [ 'mwf800', 'sb382', 'norman' ],
-//   cs112: [ 'mwf900', 'nh253', 'norman' ],
-//   cs212: [ 'mwf800', 'nh253', 'vanderlinden' ],
-//   cs214: [ 'mwf900', 'sb382', 'adams' ]
+//   cs108: { time: 'mwf800', room: 'sb382', faculty: 'norman' },
+//   cs112: { time: 'mwf900', room: 'sb382', faculty: 'adams' },
+//   cs212: { time: 'mwf900', room: 'nh253', faculty: 'norman' },
+//   cs214: { time: 'mwf800', room: 'nh253', faculty: 'adams' }
 // } // or something similar.
